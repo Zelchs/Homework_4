2fc8d322207d0adfa51f39445cfeb074d29e0660
Raivis Zelcs <raivis0211@gmail.com>
2020-06-16 23:02:01 +0300

    login

--- public/index.php
+++ public/index.php
@@ -1,12 +1,12 @@
 <?php
 include "config/config.php";
-include "src/templates/header.php";
+include "src/header.php";
 ?>
 <main>
-<p>You are logged out!</p>
+
 <p>You are logged out!</p>
 </main>
 
 <?php
-include "src/templates/footer.php";
+include "src/footer.php";
 ?>
--- public/signupForm.php
+++ public/signupForm.php
@@ -1,16 +1,16 @@
 <?php
-include "src/templates/header.php";
+include "src/header.php";
 ?>
 <main>
 <h1>Signup</h1>
-<form action="src/templates/signup.php" method="post">
+<form action="src/signup.php" method="post">
     <input type="text" name="username" placeholder="Username">
-    <input type="text" name="email" placeholder="E-mail">
+    <input type="email" name="email" placeholder="E-mail">
     <input type="password" name="pwd" placeholder="Password">
     <button type="submit" name="signup-submit">Signup</button>
 </form>
 </main>
 
 <?php
-include "src/templates/footer.php";
+include "src/footer.php";
 ?>
