60d2b85d0410c141a1b80312af2977637e4440ba
Raivis Zelcs <raivis0211@gmail.com>
2020-06-16 19:32:46 +0300

    signup form added

--- public/index.php
+++ public/index.php
@@ -1,10 +1,12 @@
 <?php
-require_once "../config/config.php";
+
 require_once "../src/templates/header.php";
 ?>
 <main>
-
+<p>You are logged !</p>
+<p>You are logged out!</p>
 </main>
 
 <?php
 require_once "../src/templates/footer.php";
+?>
---
+++ public/signupForm.php
@@ -0,0 +1,16 @@
+<?php
+require_once "../src/templates/header.php";
+?>
+<main>
+<h1>Signup</h1>
+<form action="../src/templates/signup.php" method="post">
+    <input type="text" name="username" placeholder="Username">
+    <input type="text" name="email" placeholder="E-mail">
+    <input type="password" name="pwd" placeholder="Password">
+    <button type="submit" name="signup-submit">Signup</button>
+</form>
+</main>
+
+<?php
+require_once "../src/templates/footer.php";
+?>
