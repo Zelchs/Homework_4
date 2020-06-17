b9a9aa2c0b33c6db8f63626f7950dc0ee3dfe733
Raivis Zelcs <raivis0211@gmail.com>
2020-06-17 13:10:34 +0300

    required

--- public/signupForm.php
+++ public/signupForm.php
@@ -4,9 +4,9 @@ include "src/header.php";
 <main>
 <h1>Signup</h1>
 <form action="src/signup.php" method="post">
-    <input type="text" name="username" placeholder="Username">
-    <input type="email" name="email" placeholder="E-mail">
-    <input type="password" name="pwd" placeholder="Password">
+    <input type="text" name="username" placeholder="Username" required>
+    <input type="email" name="email" placeholder="E-mail" required>
+    <input type="password" name="pwd" placeholder="Password" required>
     <button type="submit" name="signup-submit">Signup</button>
 </form>
 </main>
