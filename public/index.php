7afe71c15a6003802b8d291bd478e8268bab4ab1
Raivis Zelcs <raivis0211@gmail.com>
2020-06-18 09:08:42 +0300

    showing data upon login

--- public/index.php
+++ public/index.php
@@ -6,31 +6,20 @@ echo "<hr>";
 <main>
 
 <?php
-
+$conn = new mysqli($servername, $username, $password, $dbname);
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
 if (isset($_SESSION['user'])) {
 
 echo "<p>You are logged in!</p>";
 include "newexercise.php";
 echo "<hr>";
 include "filterForm.php";
-}
-else {
-echo "<p>You are logged out!</p>";
-}
-$conn = new mysqli($servername, $username, $password, $dbname);
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-}
-echo "Connected successfully";
-
-?>
-<?php
-
-//FIXME !! NOT SAFE need prepared statements!! reme
 if (isset($_GET['exercise'])) {
-    $aName = "%" . $_GET['exercise'] . "%"; // we add %s to get fuzzy matches
+    $aName = "%" . $_GET['exercise'] . "%"; 
     $stmt = $conn->prepare("SELECT * FROM todo WHERE exercise LIKE (?)");
-    $stmt->bind_param("s", $aName); //s means string here
+    $stmt->bind_param("s", $aName);
     $stmt->execute();
     $result = $stmt->get_result();
 } else {
@@ -39,40 +28,33 @@ if (isset($_GET['exercise'])) {
 }
 
 if ($result->num_rows > 0) {
-    echo "Cool we got " . $result->num_rows . " rows of data!<hr>";
-    // output data of each row
+    echo "<h2>Your exercises</h2>";
     while ($row = $result->fetch_assoc()) {
-        // var_dump($row);
-        // echo "id: " . $row["id"] . " name - " . $row["name"] . " artist: " . $row["artist"] . " Created on:" . $row["created"]
-        $html = "id: " . $row["id"]; //set $html text here
-        $html .= "Exercise - " . $row["exercise"]; //we add this line to previous $html
+        $html = "id: " . $row["id"]; 
+        $html .= "Exercise - " . $row["exercise"]; 
         $html .= "Notes: " . $row["note"];
         $html .= "To be done:" . $row["tododate"];
         $html .= "<hr>";
         echo $html;
-        //   echo "id: " . $row["id"]. " - Name: " . $row["name"]. " " . $row["lastname"]. "<br>";
     }
-} else {
+} 
+else {
     echo "0 results";
 }
-
-// $sql = "SELECT * FROM todo WHERE exercise LIKE 'ABBA'";
-// $result = $conn->query($sql);
-
-// we can get all results at once and then loop through them
-// $allrows = $result->fetch_all(MYSQLI_BOTH);
-$allrows = $result->fetch_all(MYSQLI_ASSOC);
-var_dump($allrows);
-foreach ($allrows as $rowindex => $row) {
-    echo "<div class='myrow' id='row-$rowindex'>";
-    // var_dump($row);
-    $html = "id: " . $row["id"]; //set $html text here
-    $html .= "Exercise - " . $row["exercise"]; //we add this line to previous $html
-    $html .= "Note: " . $row["note"];
-    $html .= "To be done:" . $row["tododate"];
-    // $html .= "<hr>";
-    echo $html;
-    echo "</div>";
+// $allrows = $result->fetch_all(MYSQLI_ASSOC);
+//     foreach ($allrows as $rowindex => $row) {
+//         echo "<div class='myrow' id='row-$rowindex'>";
+//         $html = "id: " . $row["id"]; //set $html text here
+//         $html .= "Exercise - " . $row["exercise"]; //we add this line to previous $html
+//         $html .= "Note: " . $row["note"];
+//         $html .= "To be done:" . $row["tododate"];
+//         // $html .= "<hr>";
+//         echo $html;
+//         echo "</div>";
+//     }
+}
+else {
+echo "<p>You are logged out!</p>";
 }
 ?>
 </main>
